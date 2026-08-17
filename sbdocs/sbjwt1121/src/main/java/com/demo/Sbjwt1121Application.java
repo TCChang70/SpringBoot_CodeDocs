e20013dc23eb3def540b702a0dc5803232957483
DESKTOP-P4UUKAO\student <zvincentchang@yahoo.com.tw>
2026-08-17 17:36:55 +0800

    Add Basic jwt Code and Doc

---
+++ sbdocs/sbjwt1121/src/main/java/com/demo/Sbjwt1121Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbjwt1121Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbjwt1121Application.class, args);
+	}
+
+}
---
+++ sbdocs/sbjwt1121/src/main/java/com/demo/controller/JwtUtility.java
@@ -0,0 +1,45 @@
+package com.demo.controller;
+
+import java.util.*;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtUtility {
+    private static final String SECRET = "MySecretKey"; // 密鑰，實際專案應存放在配置檔中
+
+    // 生成 Token
+    public static String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 600000)) // 有效期 10 分鐘
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+    }
+
+    // 驗證 Token
+    public static boolean validateToken(String token) {
+        try {
+            String name = Jwts.parser()
+                    .setSigningKey(SECRET)
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
+            return name != null;
+        } catch (Exception e) {
+            System.out.println("validateToken error " + e.getMessage());
+            return false;
+        }
+    }
+
+    // 從 Token 提取使用者名稱
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
